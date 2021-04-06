b7d5dace3d18c339253e148300b35155dc90c580
Joao Benevides <jrsbenevides@usp.br>
2021-04-06 16:54:02 -0300

    rev2 teste

--- src/pubcmdglobal.py
+++ src/pubcmdglobal.py
@@ -2,22 +2,35 @@
 # license removed for brevity
 
 import rospy
-from geometry_msgs.msg import PoseStamped, TransformStamped, PoseArray, Twist, Vector3
+from geometry_msgs.msg import PoseStamped, TransformStamped, PoseArray, Twist, Vector3,Pose
 
 def ControlDecision():
-    pubCmdGlb = rospy.Publisher("cmd_vel_global", Twist, queue_size=1)
+    pubCmdGlb = rospy.Publisher("cmd_global", PoseArray, queue_size=1)
     rospy.init_node('global_controller', anonymous=True)
     rate = rospy.Rate(20) # 20Hz
     while not rospy.is_shutdown():
         
-        cmdglb = Twist()
 
-        cmdglb.linear.x = 1.0
-        cmdglb.linear.y = 1.0
-        cmdglb.linear.z = 0.0
+        ps = PoseArray()
+   
+        ps.header.frame_id = "/base_link"
+        ps.header.stamp = rospy.Time.now()
+   
+        cmdglb = Pose()
+
+        cmdglb.position.x = 0.1
+        cmdglb.position.y = 0.1
+        cmdglb.position.z = 0.1
+
+        cmdglb.orientation.w = 0
+        cmdglb.orientation.x = 0
+        cmdglb.orientation.y = 0 #takeoff instruction
+        cmdglb.orientation.z = 0 #land instruction
+
+        ps.poses.append(cmdglb)
 
         #EXECUTA TODA VEZ QUE CHEGA UPDATE NA IMU
-        pubCmdGlb.publish(cmdglb)
+        pubCmdGlb.publish(ps)
         rate.sleep()
 
 if __name__ == '__main__':
